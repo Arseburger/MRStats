754db334f1d22b9f5b1c8583b60ac77d65d54879
ArseBurger <japaneesegena@gmail.com>
2025-05-07 15:26:47 +0300

    Initial Commit

---
+++ MRStats/MVVM/View/Detail/DetailHeroView.swift
@@ -0,0 +1,14 @@
+//
+    
+
+import SwiftUI
+
+struct DetailHeroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailHeroView()
+}
